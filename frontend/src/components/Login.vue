3a7de023cb384b911497a913f97aa8dcd9c701ae
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2024-06-08 18:58:50 -0400

    Text field iniciar con correo

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,9 +3,15 @@
     <v-img src="https://intranet.ubiobio.cl/c100c0d63e8ca449b605510299f54303/img/ubb_logo_new.png" alt="Logo"></v-img>
       <h2> </h2>
     <v-form>
-      <v-text-field v-model="username" label="Nombre de usuario" required></v-text-field>
+      <v-label>Iniciar sesión con credenciales institucionales</v-label>
+      <v-text-field v-model="username" label="Rut" required></v-text-field>
       <v-text-field v-model="password" label="Contraseña" type="password" required></v-text-field>
       <v-btn type="submit" color="primary">Iniciar sesión</v-btn>
+    </v-form>
+    <v-form>
+      <v-label>o</v-label>
+      <v-text-field v-model="email" label="Correo institucional" required></v-text-field>
+      <v-btn @click="login" color="primary">Enviar</v-btn>
     </v-form>
   </v-container>
 </template>

72b0ebbff1082ac155551e2fb37cd6b44608fe5c
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2024-06-25 21:55:24 -0400

    Ajuste barra de busqueda y v-card de grupos

--- frontend/src/components/AdminHome.vue
+++ frontend/src/components/AdminHome.vue
@@ -1,43 +1,96 @@
 <template>
-    <v-card
-      class="mx-auto"
-      max-width="400"
-    >
-      <v-img
-        class="align-end text-white"
-        height="200"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-        cover
-      >
-        <v-card-title>Top 10 Australian beaches</v-card-title>
-      </v-img>
-  
-      <v-card-subtitle class="pt-4">
-        Number 10
-      </v-card-subtitle>
-  
-      <v-card-text>
-        <div>Whitehaven Beach</div>
-  
-        <div>Whitsunday Island, Whitsunday Islands</div>
-      </v-card-text>
-  
-      <v-card-actions>
-        <v-btn color="orange" text="Share"></v-btn>
-  
-        <v-btn color="orange" text="Explore"></v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-container class="Busqueda">
+  <v-card class="pa-4" height="100px" img="https://cdn.vuetifyjs.com/images/toolbar/map.jpg" flat>
+    <v-toolbar dense floating class="search-bar">
+      <v-text-field prepend-icon="mdi-magnify" hide-details single-line></v-text-field>
+      <v-btn icon>
+        <v-icon>mdi-crosshairs-gps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</v-container>
+<v-container>
+    <v-row>
+      <v-col cols="12" md="6" lg="3" v-for="(item, index) in items" :key="index">
+        <v-card class="mx-auto" max-width="400">
+          <v-img class="align-end text-white" height="200" :src="item.img" cover>
+            <v-card-title>{{ item.title }}</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pt-4">
+            {{ item.subtitle }}
+          </v-card-subtitle>
+
+          <v-card-text>
+            <div>{{ item.description1 }}</div>
+            <div>{{ item.description2 }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text="Share"></v-btn>
+            <v-btn color="orange" text="Explore"></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
   </template>
-<script>
 
-export default {
-  name: 'AdminHome',
-  data: () => ({
-
-  }),
-  methods: {
-  },
+<style scoped>
+.search-bar {
+  margin-top: 3000;
 }
+.Busqueda {
+  margin-top: 30px;
+}
+</style>
 
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Top 10 Australian beaches",
+          subtitle: "Number 10",
+          description1: "Whitehaven Beach",
+          description2: "Whitsunday Island, Whitsunday Islands",
+          img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        },
+        {
+          title: "Top 10 Australian beaches",
+          subtitle: "Number 10",
+          description1: "Whitehaven Beach",
+          description2: "Whitsunday Island, Whitsunday Islands",
+          img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        },
+        {
+          title: "Top 10 Australian beaches",
+          subtitle: "Number 10",
+          description1: "Whitehaven Beach",
+          description2: "Whitsunday Island, Whitsunday Islands",
+          img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        },
+        {
+          title: "Top 10 Australian beaches",
+          subtitle: "Number 10",
+          description1: "Whitehaven Beach",
+          description2: "Whitsunday Island, Whitsunday Islands",
+          img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        },
+        {
+          title: "Top 10 Australian beaches",
+          subtitle: "Number 10",
+          description1: "Whitehaven Beach",
+          description2: "Whitsunday Island, Whitsunday Islands",
+          img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        },
+        // Agrega más objetos aquí para cada tarjeta que desees mostrar
+      ],
+    };
+  },
+};
 </script>

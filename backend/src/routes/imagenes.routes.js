5b2897d3636d56ec5c8d55373b523ae7fe34df2c
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2024-07-24 18:10:23 -0400

    fix funcion async de imagenes y formulario en la exportacion de los modulos

--- backend/src/routes/imagenes.routes.js
+++ backend/src/routes/imagenes.routes.js
@@ -2,7 +2,7 @@ const fastify = require('../config/configFastify.js');
 
 const { obtenerImagenes, obtenerImagenPorID, crearImagen, eliminarImagen } = require('../controllers/imagenes.controller.js');
 
-module.exports = async function (fastify, opts, done) {
+module.exports = function (fastify, opts, done) {
     fastify.get('/imagen', obtenerImagenes);
     fastify.get('/imagen/:id', obtenerImagenPorID);
     fastify.post('/imagen', crearImagen);

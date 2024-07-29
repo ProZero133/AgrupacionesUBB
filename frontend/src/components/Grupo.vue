3e57a7e9fe656398eb2cb342327fd6914103216a
Nicolas Madrid <nicolas.madrid2001@alumnos.ubiobio.cl>
2024-07-29 00:21:18 -0400

    cambios en botones de agrupacion

--- frontend/src/components/Grupo.vue
+++ frontend/src/components/Grupo.vue
@@ -353,8 +353,6 @@ export default {
     items: [
       { title: 'Crear Actividad', path: '/api/crear_actividad/' },
       { title: 'Crear Publicación', path: '/api/crear_publicacion/' },
-      { title: 'Ver solicitudes', path: '/api/solicitudes_agrupacion/' },
-      { title: 'Ver miembros', path: '/api/administrar_roles_agrupaciones/' },
     ],
 
     modItems: [

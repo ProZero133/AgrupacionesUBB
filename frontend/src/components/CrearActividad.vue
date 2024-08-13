9932fa3fa940ea5f6eec691c060d495b61efd8c6
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2024-08-13 19:01:51 -0400

    dialog cupos y fecha actividad, slider enves de checkbox para tipo actividad

--- frontend/src/components/CrearActividad.vue
+++ frontend/src/components/CrearActividad.vue
@@ -4,112 +4,101 @@
   <v-container cols="12">
 
     <v-card class="mx-auto px-6 py-8" max-width="800">
-      <v-form @submit.prevent="CreaActividad(nom_act, descripcion, imagen, tipo, cupos)" fast-fail class="form"
-        ref="formulario" v-model="formValid">
-        <v-row>
-          <v-spacer></v-spacer>
-          <v-col cols="12" justify="center" class="title-card">
-            <h1 class="texto">Crear actividad</h1>
-            <p class="text-left">Por favor, introducir abajo los datos correspondientes a la actividad a crear.</p>
+    <v-form @submit.prevent="dialog = true" fast-fail class="form" ref="formulario" v-model="formValid">
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="12" justify="center" class="title-card">
+          <h1 class="texto">Crear actividad</h1>
+          <p class="text-left">Por favor, introducir abajo los datos correspondientes a la actividad a crear.</p>
+        </v-col>
+
+        <v-col cols="12" md="7" class="nomdes">
+          <v-col cols="12">
+            <v-text-field class="nombreAct" v-model="nom_act" label="Nombre de la actividad" clearable required
+              variant="solo-filled" :rules="nombreRules"></v-text-field>
           </v-col>
 
-          <v-col cols="12" md="7" class="nomdes">
-
-            <v-col cols="12">
-              <v-text-field class="nombreAct" v-model="nom_act" label="Nombre de la actividad" clearable required
-                variant="solo-filled" :rules="nombreRules"></v-text-field>
-            </v-col>
-
-            <v-col cols="12">
-              <v-textarea class="descripcionAct" v-model="descripcion" label="Descripción de la actividad" clearable
-                required variant="solo-filled" rows="10" no-resize :rules="descrules" counter>
-              </v-textarea>
-            </v-col>
-
+          <v-col cols="12">
+            <v-textarea class="descripcionAct" v-model="descripcion" label="Descripción de la actividad" clearable
+              required variant="solo-filled" rows="10" no-resize :rules="descrules" counter>
+            </v-textarea>
           </v-col>
+        </v-col>
 
-          <v-col cols="12" md="5">
+        <v-col cols="12" md="5">
+          <v-col cols="12">
+            <v-file-input
+              v-model="imagen"
+              accept="image/png, image/jpeg, image/bmp"
+              label="Imagen para la actividad"
+              clearable
+              required
+              variant="solo-filled"
+              prepend-icon=""
+              :rules="imgRules"
+              @change="onFileChange($event)"
+              @click:clear="urlImagen = defaultImageUrl">
+            </v-file-input>
+          </v-col>
+          <v-col>
+            <v-img
+              class="image"
+              max-height="280px"
+              aspect-ratio="1"
+              :src='urlImagen'
+              :rules="imgRules"
+              />
+          </v-col>
+        </v-col>
 
-            <v-col cols="12">
-              <v-file-input
-                v-model="imagen"
-                accept="image/png, image/jpeg, image/bmp"
-                label="Imagen para la actividad"
-                clearable
-                required
-                variant="solo-filled"
-                prepend-icon=""
-                :rules="imgRules"
-                @change="onFileChange($event)"
-                @click:clear="urlImagen = defaultImageUrl">
-              </v-file-input>
-            </v-col>
+        <v-col cols="12" md="12">
+          <v-col cols="12" class="bottomElement">
+            <v-switch v-model="tipo" :disabled="verificado !== 'Verificado'" inset color="green">
+              <template v-slot:label>
+                <div>
+                  <h2 v-if=tipo>Actividad Pública</h2>
+                  <h2 v-else>Actividad Privada</h2>
+                  <h4 v-if=tipo>Todos podrán ver y participar en la actividad, sean miembros del grupo o no.</h4>
+                  <h4 v-else>Sólo los miembros del grupo podrán ver y participar de la actividad.</h4>
+                  <h4 v-if="verificado !== 'Verificado'" class="warning-message">Debes acreditar tu grupo para crear
+                    actividades públicas.</h4>
+                </div>
+              </template>
+            </v-switch>
+          </v-col>
+          <v-col cols="4">
+            <v-btn class="form-submit" type="submit" color="#2CA2DC" :disabled=!formValid>Crear actividad</v-btn>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-form>
 
+    <!-- Dialog for date and cupos -->
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Detalles de la Actividad</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="dialogForm">
             <v-col>
-              <v-img
-                class="image"
-                max-height="280px"
-                aspect-ratio="1"
-                :src='urlImagen'
-                :rules="imgRules"
-                />
+              <v-text-field v-model="date" label="Fecha para la actividad" type="datetime-local" required :min="hoy"
+                :rules="dateRules" :error-messages="dateErrors"></v-text-field>
             </v-col>
-
-          </v-col>
-
-          <v-col cols="12" md="7" class="botcosos">
-
-            <v-col>
-                <v-text-field v-model="date" label="Fecha para la actividad" type="datetime-local" required :min="hoy"
-                :rules="dateRules" :error-messages="dateErrors" @change="validateDate"></v-text-field>
-            </v-col>
-
-          </v-col>
-
-          <v-col cols="12" md="5" class="botcosos">
-
             <v-col>
               <v-text-field class="Cupos" v-model="cupos" label="Cupos para la actividad" type="number"
-              min="1" required :rules="cuposRules"></v-text-field>
+                min="1" required :rules="cuposRules"></v-text-field>
             </v-col>
-
-          </v-col>
-          
-
-          <v-col cols="12" md="12">
-
-            <v-col cols="12" class="bottomElement">
-
-              <v-checkbox v-model="tipo" :disabled="verificado !== 'Verificado'">
-                <template v-slot:label>
-                  <div>
-                    <h2 v-if=tipo>Actividad Pública: Activado</h2>
-                    <h2 v-else>Actividad Pública: Desactivado</h2>
-                    <h4 v-if=tipo>Todos podrán ver y participar en la actividad, sean miembros del grupo o no.</h4>
-                    <h4 v-else>Sólo los miembros del grupo podrán ver y participar de la actividad.</h4>
-                    <h4 v-if="verificado !== 'Verificado'" class="warning-message">Debes acreditar tu grupo para crear
-                      actividades públicas.</h4>
-                  </div>
-                </template>
-              </v-checkbox>
-
-            </v-col>
-
-            <!-- <v-col cols="12">
-              <v-text-field class="idAct" v-model="id_agr" label="ID de la agrupación" clearable counter required>
-              </v-text-field>
-            </v-col> -->
-
-            <v-col cols="4">
-              <v-btn class="form-submit" type="submit" color="#2CA2DC" :disabled=!formValid>Crear actividad</v-btn>
-            </v-col>
-
-          </v-col>
-
-        </v-row>
-
-      </v-form>
-    </v-card>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Cancelar</v-btn>
+          <v-btn color="blue darken-1" text @click="CreaActividad(nom_act, descripcion, imagen, tipo, cupos, date)">Aceptar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-card>
   </v-container>
 </template>
 
@@ -179,6 +168,8 @@ export default {
     defaultImageUrl: addImage,
     urlImagen: addImage,
     idImagen: '',
+    dialog: false,
+    formValid: false,
     verificado: '',
     cupos: 1,
     date: null,
@@ -211,16 +202,6 @@ export default {
             }
           }
       return null;
-    },
-
-    validateDate() {
-      this.dateErrors = [];
-      for (let rule of this.dateRules) {
-          let result = rule(this.datosDeuda.fechaVencimiento);
-          if (typeof result === 'string') {
-            this.dateErrors.push(result);
-          }
-        }
     },
 
     createImage(file) {
